304037805f1efb202760ff007955d5dd1efe3ff4
Toke Eskildsen <te@ekot.dk>
2014-11-21 16:16:57 +0100

    Working Proof of Concept, DocValuing a single field in unit test

--- src/main/java/dk/statsbiblioteket/netark/dvenabler/TransformingDirectoryReader.java
+++ src/main/java/dk/statsbiblioteket/netark/dvenabler/TransformingDirectoryReader.java
@@ -14,13 +14,18 @@
  */
 package dk.statsbiblioteket.netark.dvenabler;
 
+import com.sun.swing.internal.plaf.metal.resources.metal_sv;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.logging.Log;
 import org.apache.lucene.index.*;
 import org.apache.lucene.queryparser.xml.FilterBuilderFactory;
 import org.apache.lucene.store.FilterDirectory;
+import org.apache.lucene.util.BytesRef;
+import org.apache.lucene.util.BytesRefArray;
+import org.eclipse.jetty.deploy.bindings.OrderedGroupBinding;
 
 import java.io.IOException;
+import java.util.*;
 
 /**
  *
@@ -75,8 +80,12 @@ public class TransformingDirectoryReader extends FilterDirectoryReader {
         @Override
         public SortedDocValues getSortedDocValues(String field) throws IOException {
             SortedDocValues dv = super.getSortedDocValues(field);
-            log.info("getSortedDocValues called for field '" + field + "'. Has DV: " + (dv != null));
-            return dv;
+            if (dv != null) {
+                log.info("getSortedDocValues called for field '" + field + "'. DV already present, returning directly");
+                return dv;
+            }
+
+            return new SortedDocValuesWrapper(this, field);
         }
 
         @Override
@@ -93,4 +102,64 @@ public class TransformingDirectoryReader extends FilterDirectoryReader {
         }
     }
 
+    /**
+     * Memory-intensive transformer: All values are stored as String in (gasp) a Set and a List.
+     */
+    public static class SortedDocValuesWrapper extends SortedDocValues {
+        private final AtomicReader reader;
+        private final String field;
+        private final Set<String> FIELDS;
+        private final List<String> values;
+
+        public SortedDocValuesWrapper(AtomicReader reader, String field) throws IOException {
+            this.reader = reader;
+            this.field = field;
+            FIELDS = new HashSet<String>(Arrays.asList(field));
+            log.info("Creating map for SortedDocValues for field '" + field + "'");
+            long startTime = System.nanoTime();
+            values = fill();
+            log.info("Finished creating SortedDocValues with " + values.size() + " unique values for field '" + field
+                     + "' in " + ((System.nanoTime()-startTime)/1000000/1000) + "ms");
+        }
+
+        private List<String> fill() throws IOException {
+            final SortedSet<String> values = new TreeSet<String>();
+            for (int docID = 0 ; docID < reader.maxDoc() ; docID++) {
+                String value = reader.document(docID, FIELDS).get(field);
+                if (value != null) {
+                    values.add(value);
+                }
+            }
+            return new ArrayList<String>(values);
+        }
+
+        @Override
+        public int getOrd(int docID) {
+            try {
+                String value = reader.document(docID, FIELDS).get(field);
+                if (value == null) {
+                    return -1;
+                }
+                int ord = Collections.binarySearch(values, value);
+                if (ord < 0) {
+                    throw new IllegalStateException(
+                            "The ord for value '" + value + "' for docID " + docID + " in field '" + field
+                            + "' could not be located but should always be present");
+                }
+                return ord;
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to lookup docID=" + docID + ", field=" + field, e);
+            }
+        }
+
+        @Override
+        public void lookupOrd(int ord, BytesRef result) {
+            result.copyChars(values.get(ord));
+        }
+
+        @Override
+        public int getValueCount() {
+            return values.size();
+        }
+    }
 }
