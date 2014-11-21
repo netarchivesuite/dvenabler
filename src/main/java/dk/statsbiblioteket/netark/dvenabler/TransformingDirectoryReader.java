0dfe635b2a06a2836b687ab09e7b8fdd0c6ff787
Toke Eskildsen <te@ekot.dk>
2014-11-21 12:52:46 +0100

    Skeleton unit test added

---
+++ src/main/java/dk/statsbiblioteket/netark/dvenabler/TransformingDirectoryReader.java
@@ -0,0 +1,30 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package dk.statsbiblioteket.netark.dvenabler;
+
+import dk.statsbiblioteket.util.qa.QAInfo;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
+
+/**
+ *
+ */
+@QAInfo(level = QAInfo.Level.NORMAL,
+        state = QAInfo.State.IN_DEVELOPMENT,
+        author = "te")
+public class TransformingDirectoryReader {
+    private static Log log = LogFactory.getLog(TransformingDirectoryReader.class);
+
+}
